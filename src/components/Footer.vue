3400bdbc18df724d65ef62f7d2d3625779c0320a
Qianwen <qianwenzhangnancy@mail.wit.ie>
2020-04-19 04:33:09 +0100

    footer edition

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  <b-container class="donation-footer">
+    <p style="color:white; font-weight: bold">produce by Qianwen Zhang @ 2020</p>
+  </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'footer'
+}
+</script>
+
+<style scoped>
+  .donation-footer {
+    text-align: center;
+    width: 100%;
+    background-color: black;
+  }
+</style>
